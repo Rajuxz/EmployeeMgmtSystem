433ad348ad4cf94073ad0f4e3c7f1b7723c0da5e
Rajuxz <rajankhanalxz@gmail.com>
2024-01-08 23:26:22 +0545

    Conf: Modifying Viewmodel by adding string field to store image address.

--- EmployeeMgmtSystem/Models/EmployeeViewModel.cs
+++ EmployeeMgmtSystem/Models/EmployeeViewModel.cs
@@ -9,5 +9,7 @@
         public string? Position { get; set; }
         public string? Email { get; set; }
         public string? Department { get; set; }
+
+        public string? Image { get; set; }
     }
 }
