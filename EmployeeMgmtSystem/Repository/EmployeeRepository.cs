e0ff0b4b90819083d0d13c67ca5623a440513f7e
Rajuxz <rajankhanalxz@gmail.com>
2024-01-11 13:56:16 +0545

    Adding dependency to EmployeeRepository

--- EmployeeMgmtSystem/Repository/EmployeeRepository.cs
+++ EmployeeMgmtSystem/Repository/EmployeeRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace EmployeeMgmtSystem.Repository
+﻿using EmployeeMgmtSystem.DataContext;
+
+namespace EmployeeMgmtSystem.Repository
 {
     public class EmployeeRepository:IEmployee
     {
+        private readonly EmployeeDbContext _dbContext;
+        public EmployeeRepository( EmployeeDbContext dbContext) {
+            _dbContext = dbContext;
+        }
+       
     }
 }
