510e4b0b5dc8fe4c0664c7a8420c47340d1759e8
Rajuxz <rajankhanalxz@gmail.com>
2024-01-11 12:14:49 +0545

    AssignedWork model

---
+++ EmployeeMgmtSystem/Models/Domain/AssignedWork.cs
@@ -0,0 +1,14 @@
+﻿namespace EmployeeMgmtSystem.Models.Domain
+{
+    public class AssignedWork
+    {
+        public int Id { get; set; }
+        public string? Description{ get; set; }
+        public bool? IsUrgent{ get; set; } = false;
+        public string? Status { get; set; } //Normal/Urgent
+        public bool? IsCompleted { get; set; } = false;
+
+        public int EmployeeId { get; set; }
+        public EmployeeModel? Employee { get; set; }
+    }
+}
