367f47e7297558637203e1d340fafa2de8459738
Rajuxz <rajankhanalxz@gmail.com>
2024-01-12 12:25:41 +0545

    Implementation of Default Repository

---
+++ EmployeeMgmtSystem/Repository/Implementation/Repository.cs
@@ -0,0 +1,48 @@
+﻿using EmployeeMgmtSystem.DataContext;
+using EmployeeMgmtSystem.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace EmployeeMgmtSystem.Repository.Implementation
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly EmployeeDbContext _dbContext;
+        internal DbSet<T> database;
+        public Repository(EmployeeDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            database = _dbContext.Set<T>();
+
+            //For example: _dbContext.EmployeeTable == database. 
+        }
+
+        public void Add(T entity)
+        {
+            database.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = database;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = database;
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            database.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            database.RemoveRange(entities);
+        }
+    }
+}
