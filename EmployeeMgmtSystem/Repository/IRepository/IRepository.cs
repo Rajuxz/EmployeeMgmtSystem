1a541a0cb48fc7f7ef7e866267effd8f76b9a557
Rajuxz <rajankhanalxz@gmail.com>
2024-01-12 12:27:31 +0545

    Base repository for Project.

---
+++ EmployeeMgmtSystem/Repository/IRepository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace EmployeeMgmtSystem.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        void Add(T entity);
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool>> filter);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
