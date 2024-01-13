42c414eda2f869dc638102e1027ef69546381d55
Rajuxz <rajankhanalxz@gmail.com>
2024-01-13 22:28:13 +0545

    Rewriting all the functionalities using Repository Pattern.

--- EmployeeMgmtSystem/Controllers/EmployeeController.cs
+++ EmployeeMgmtSystem/Controllers/EmployeeController.cs
@@ -38,5 +38,92 @@ namespace EmployeeMgmtSystem.Controllers
                 return View(ex);
             }
         }
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddEmployee(EmployeeViewModel employeeVm)
+        {
+            try
+            {
+                string filename = "default.png";
+                if (employeeVm.Image != null)
+                { 
+                    string uploadFolder = Path.Combine(_webHostEnv.WebRootPath, "images");
+                    filename = Guid.NewGuid().ToString() + employeeVm.Image.FileName;
+                    string filePath = Path.Combine(uploadFolder, filename);
+                    employeeVm.Image.CopyTo(new FileStream(filePath, FileMode.Create));
+
+                }
+                var employee = new EmployeeModel()
+                {
+                    Name = employeeVm.Name,
+                    Address = employeeVm.Address,
+                    Salary = employeeVm.Salary,
+                    Phone = employeeVm.Phone,
+                    Position = employeeVm.Position,
+                    Email = employeeVm.Email,
+                    Department = employeeVm.Department,
+                    Image = filename
+                };
+                _employeeRepo.Add(employee);
+                _employeeRepo.Save();
+                TempData["SuccessMessage"] = "Employee Data Added Successfully.";
+                return RedirectToAction("Employees");
+
+            }catch(Exception e)
+            {
+                TempData["ErrorMessage"] = $"Cannot add data. {e.ToString()}";
+                return RedirectToAction("Employees");
+            }
+            
+        }
+        public IActionResult UpdateEmployee(int id)
+        {
+            var employee = _employeeRepo.Get(x => x.Id == id);
+            if (employee != null)
+            {
+                var employeeData = new UpdateEmployeeViewModel()
+                {
+                    Id = employee.Id,
+                    Name = employee.Name,
+                    Address = employee.Address,
+                    Salary = employee.Salary,
+                    Phone = employee.Phone,
+                    Position = employee.Position,
+                    Email = employee.Email,
+                    Department = employee.Department,
+                };
+                return View(employeeData);
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Opps !! Data Not Found. ";
+                return RedirectToAction("Employees");
+            }
+        }
+        [HttpPost]
+        public IActionResult UpdateEmployee(UpdateEmployeeViewModel updateVM)
+        {
+            try
+            {
+
+                if (ModelState.IsValid)
+                {
+                    if(updateVM.Image != null)
+                    {
+                        string uploadFolder = Path.Combine(_webHostEnv.WebRootPath, "images");
+                        string filename = Guid.NewGuid().ToString() + updateVM.Image.FileName;
+                        string filePath = Path.Combine(uploadFolder, filename);
+                        updateVM.Image.CopyTo(new FileStream(filePath, FileMode.Create));
+
+                    }
+                }
+            }catch(Exception ex) {
+                TempData["ErrorMessage"] = $"Something went wrong.{ex.ToString()}";
+                return RedirectToAction("Employees");
+            }
+        }
     }
 }
