54aa7e7e698ea8cc54e7f4d3a39cca6d29e92e56
Rajuxz <rajankhanalxz@gmail.com>
2024-01-14 13:41:41 +0545

    Adding method for saving file and returning it's unique name to store in database.

--- EmployeeMgmtSystem/Repository/Implementation/EmployeeRepository.cs
+++ EmployeeMgmtSystem/Repository/Implementation/EmployeeRepository.cs
@@ -8,10 +8,12 @@ namespace EmployeeMgmtSystem.Repository.Implementation
 {
     public class EmployeeRepository : Repository<EmployeeModel>, IEmployeeRepository
     {
+        private IWebHostEnvironment _environment;
         private EmployeeDbContext _employeeDbContext;
-        public EmployeeRepository(EmployeeDbContext employeeDbContext) : base(employeeDbContext)
+        public EmployeeRepository(EmployeeDbContext employeeDbContext, IWebHostEnvironment environment) : base(employeeDbContext)
         {
             _employeeDbContext = employeeDbContext;
+            _environment = environment;
         }
 
         public void Save()
@@ -19,11 +21,33 @@ namespace EmployeeMgmtSystem.Repository.Implementation
             _employeeDbContext.SaveChanges();
         }
 
+        public string? SaveFileAndReturnName(string path, IFormFile file)
+        {
+            try
+            {
+                if (file != null)
+                {
+                    string uniqueName = Guid.NewGuid().ToString() + file.FileName;
+                    string serverLocation = Path.Combine(_environment.WebRootPath, Path.Combine(path, uniqueName));
+                    file.CopyTo(new FileStream(serverLocation, FileMode.Create));
+                    return uniqueName;
+                }
+                else
+                {
+                    return "default.png";
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception saving file: {ex.Message}");
+                return "default.png";
+            }
+        }
 
         public void Update(EmployeeModel model)
         {
             _employeeDbContext.Employees?.Update(model);
         }
-      
+
     }
 }
