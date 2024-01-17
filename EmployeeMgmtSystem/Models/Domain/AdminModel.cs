6e7e1e3afde8f396aac7da389174ce05d3633129
Rajuxz <rajankhanalxz@gmail.com>
2024-01-17 16:29:57 +0545

    Admin Model

---
+++ EmployeeMgmtSystem/Models/Domain/AdminModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeMgmtSystem.Models.Domain
+{
+    public class AdminModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
