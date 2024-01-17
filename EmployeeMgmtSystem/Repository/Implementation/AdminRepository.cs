5e8b81222022aa34fbd7cb059e5ffe2f454789d1
Rajuxz <rajankhanalxz@gmail.com>
2024-01-17 23:18:41 +0545

    Implementing Repository for admin access.

---
+++ EmployeeMgmtSystem/Repository/Implementation/AdminRepository.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeMgmtSystem.DataContext;
+using EmployeeMgmtSystem.Models.Domain;
+using EmployeeMgmtSystem.Repository.IRepository;
+
+namespace EmployeeMgmtSystem.Repository.Implementation
+{
+	public class AdminRepository : Repository<AdminModel>, IAdminRepository
+	{
+		public AdminRepository(EmployeeDbContext dbContext) : base(dbContext)
+		{
+		}
+	}
+}
