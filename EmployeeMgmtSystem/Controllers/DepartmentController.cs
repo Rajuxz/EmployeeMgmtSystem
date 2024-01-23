56bb3b7518acfa2664926c08180234fcdab5dfd1
Rajuxz <rajankhanalxz@gmail.com>
2024-01-23 22:01:00 +0545

    Department related code will go here.

---
+++ EmployeeMgmtSystem/Controllers/DepartmentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeMgmtSystem.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public class DepartmentController : Controller
+    {
+       public IActionResult AddDepartment()
+        {
+            return View();
+        }
+    }
+}
