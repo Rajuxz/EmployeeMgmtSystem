50abe1027cb40790b6d965a8762de38c021d7782
Rajuxz <rajankhanalxz@gmail.com>
2024-01-23 22:02:08 +0545

    Admin View model

---
+++ EmployeeMgmtSystem/Models/Admin/AdminViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeMgmtSystem.Models.Admin
+{
+    public class AdminViewModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+
+        public string? ProfilePath { get; set; }
+        public FormFile? Profile { get; set; }
+    }
+}
