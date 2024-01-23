d8005491b314bf08ddd93f1eb86ef68953fd8afe
Rajuxz <rajankhanalxz@gmail.com>
2024-01-23 22:02:50 +0545

    Adding file saving logics.

--- EmployeeMgmtSystem/Repository/Implementation/AdminRepository.cs
+++ EmployeeMgmtSystem/Repository/Implementation/AdminRepository.cs
@@ -6,8 +6,42 @@ namespace EmployeeMgmtSystem.Repository.Implementation
 {
 	public class AdminRepository : Repository<AdminModel>, IAdminRepository
 	{
-		public AdminRepository(EmployeeDbContext dbContext) : base(dbContext)
-		{
-		}
-	}
+        private IWebHostEnvironment _environment;
+        private EmployeeDbContext _employeeDbContext;
+        public AdminRepository(EmployeeDbContext dbContext, IWebHostEnvironment environment) : base(dbContext)
+        {
+            _employeeDbContext = dbContext;
+            _environment = environment;
+        }
+        public string? SaveFileAndReturnName(string path, IFormFile file)
+           {
+              try{
+                 if (file != null)
+                 {
+                      string uniqueName = Guid.NewGuid().ToString() + file.FileName;
+                      string serverLocation = Path.Combine(_environment.WebRootPath, Path.Combine(path, uniqueName));
+                      file.CopyTo(new FileStream(serverLocation, FileMode.Create));
+                      return uniqueName;
+                 }
+                 else
+                 {
+                      return "default.png";
+                 }
+              }
+              catch (Exception ex)
+              {
+                  Console.WriteLine($"Exception saving file: {ex.Message}");
+                  return "default.png";
+              }
+            }
+        public void Save()
+        {
+            _employeeDbContext.SaveChanges();
+        }
+
+        public void Update(AdminModel admin)
+        {
+            _employeeDbContext.Update(admin);
+        }
+    }
 }
