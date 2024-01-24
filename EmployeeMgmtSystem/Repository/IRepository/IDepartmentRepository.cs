1a29a289b7ca8c57e3c838855c6584ba32d642e5
Rajuxz <rajankhanalxz@gmail.com>
2024-01-24 22:34:06 +0545

    adding signature for count method

---
+++ EmployeeMgmtSystem/Repository/IRepository/IDepartmentRepository.cs
@@ -0,0 +1,10 @@
+﻿using EmployeeMgmtSystem.Models.Domain;
+
+namespace EmployeeMgmtSystem.Repository.IRepository
+{
+    public interface IDepartmentRepository: IRepository<Department>
+    {
+        void Update(Department model);
+        void Save();
+    }
+}
