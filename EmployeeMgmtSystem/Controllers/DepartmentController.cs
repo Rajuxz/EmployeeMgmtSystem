bc3a6008707cd6ea02140ef688fad30e7bdf06b9
Rajuxz <rajankhanalxz@gmail.com>
2024-01-24 22:32:27 +0545

    Adding departments

--- EmployeeMgmtSystem/Controllers/DepartmentController.cs
+++ EmployeeMgmtSystem/Controllers/DepartmentController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using EmployeeMgmtSystem.Models.Domain;
+using EmployeeMgmtSystem.Repository.IRepository;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeMgmtSystem.Controllers
@@ -6,9 +8,56 @@ namespace EmployeeMgmtSystem.Controllers
     [Authorize(Roles ="Admin")]
     public class DepartmentController : Controller
     {
-       public IActionResult AddDepartment()
+
+        private readonly IDepartmentRepository _departmentRepo;
+        public DepartmentController(IDepartmentRepository departmentRepo)
+        {
+            _departmentRepo = departmentRepo;            
+        }
+        public IActionResult AddDepartment()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddDepartment([FromBody] Department department)
+        {
+            if(department != null)
+            {
+                var departmentObj = new Department()
+                {
+                    Name = department.Name,
+                };
+                _departmentRepo.Add(departmentObj);
+                _departmentRepo.Save();
+
+                var json = new
+                {
+                    message = "Succeed"
+                };
+                 return Json(json);
+            }
+            else
+            {
+                return Json("this");
+                
+            }
+        }
+        public IActionResult GetDepartments()
+        {
+            try
+            {
+                var department = _departmentRepo.GetAll().ToList();
+                return Json(department);
+            }
+            catch (Exception e)
+            {
+                var data = new
+                {
+                    message = e.Message 
+                };
+                return Json(data);
+            }
         }
     }
 }
