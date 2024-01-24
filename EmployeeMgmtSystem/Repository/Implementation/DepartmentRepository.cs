599b898bbc73de02d56e62314bc23bd18d0d82ae
Rajuxz <rajankhanalxz@gmail.com>
2024-01-24 22:33:21 +0545

    Department repository

---
+++ EmployeeMgmtSystem/Repository/Implementation/DepartmentRepository.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeMgmtSystem.DataContext;
+using EmployeeMgmtSystem.Models.Domain;
+using EmployeeMgmtSystem.Repository.IRepository;
+
+namespace EmployeeMgmtSystem.Repository.Implementation
+{
+    public class DepartmentRepository:Repository<Department>, IDepartmentRepository
+    {
+        private readonly EmployeeDbContext _employeeDbContext;
+        public DepartmentRepository(EmployeeDbContext employeeDbContext):base(employeeDbContext)
+        {
+            _employeeDbContext = employeeDbContext;
+        }
+
+       public void Update(Department model)
+        {
+            _employeeDbContext.Update(model);
+        }
+       public void Save()
+        {
+            _employeeDbContext.SaveChanges();
+        }
+    }
+}
