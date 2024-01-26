6e4da1bc577b7627a57a5834ffece776ea058c0d
Rajuxz <rajankhanalxz@gmail.com>
2024-01-26 11:45:27 +0545

    Adding Foreign keys to viewmodel.

--- EmployeeMgmtSystem/Models/EmployeeViewModel.cs
+++ EmployeeMgmtSystem/Models/EmployeeViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace EmployeeMgmtSystem.Models
+﻿using EmployeeMgmtSystem.Models.Domain;
+
+namespace EmployeeMgmtSystem.Models
 {
     public class EmployeeViewModel
     {
@@ -8,7 +10,8 @@
         public string? Phone { get; set; }
         public string? Position { get; set; }
         public string? Email { get; set; }
-        public string? Department { get; set; }
+        public List<Department>? Departments { get; set; }
+        public int? SelectedDepartmentId { get; set; }
 
         public IFormFile? Image { get; set; }
     }
